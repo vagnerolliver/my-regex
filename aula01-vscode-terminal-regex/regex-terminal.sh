c2eb1c5346f0b8e711865d5faeace8aa7c8c8058
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-01-09 21:05:32 -0300

    commit temp

--- aula01-vscode-terminal-regex/regex-terminal.sh
+++ aula01-vscode-terminal-regex/regex-terminal.sh
@@ -9,9 +9,19 @@
 
 #find . -name *.js -not -path '*node_module**' | ipt
 
-
 CONTENT="'use strict';"
-find . -name *.js -not -path '*node_module**' \
+find . -name *.js -not -path '*node_modules**' \
 | ipt -o \
+| xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
+/g' {file}
+
+
+# 1s -> primeira linha
+# ^-> primeira coluna
+# substitui pelo $CONTENT
+# quebrou a linha para adicionar um \n implicito
+
+# muda tudo!
+find . -name *.js -not -path '*node_modules**' \
 | xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
 /g' {file}
