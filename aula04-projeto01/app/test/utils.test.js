83a4af51e15a48b23275b889b2d9167020d1aac6
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-01-13 21:19:20 -0300

    added aula4

---
+++ aula04-projeto01/app/test/utils.test.js
@@ -0,0 +1,18 @@
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const { InvalidRegexError, evaluateRegex } = require('../src/utils')
+
+describe('Util', () => {
+
+    it('#evaluateRegex should  throw an error using an unsafe regex', () => { 
+        const unsafeRegex = /^([a-z|A-Z|0-9]+\s?)+$/
+        expect(() => evaluateRegex(unsafeRegex)).to.throw(InvalidRegexError, `This ${unsafeRegex} is unsafe dude!`)
+
+    })
+    
+    it('#evaluateRegex should not throw an error using a safe regex', () => { 
+        const safeRegex =  /^([a-z])$/
+        expect(() => evaluateRegex(safeRegex)).to.not.throw
+        expect(evaluateRegex(safeRegex)).to.be.ok
+    })
+})
